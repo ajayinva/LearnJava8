73db4d17d5d90be67f285e6251b5dd61cf2d3c50
Ajay A <ajayinva@gmail.com>
2017-07-12 13:52:02 -0400

    first commit

--- src/com/aces/learn/java8/StateService.java
+++ src/com/aces/learn/java8/StateService.java
@@ -26,22 +26,32 @@ public class StateService {
 		List<State> states = stateService.getAllStates();			
 		System.out.println(states);
 		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+		
 		System.out.println("----------All East Coast States Java 7-----------");
 		List<State> statesOnEastCoastJava7 = stateService.getStatesOnEastCoastJava7(states);	
 		System.out.println(statesOnEastCoastJava7);
 		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+		
 		System.out.println("----------All East Coast States Java 8-----------");
 		List<State> statesOnEastCoastJava8 = stateService.filter(states, State::isEastCoast);
 		System.out.println(statesOnEastCoastJava8);
 		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+		
 		System.out.println("----------All States with more than 500 Counties Java 8-----------");
 		List<State> statesMoreThan500Counties1 = stateService.filter(states, new MoreThan500CountiesPredicate());
 		System.out.println(statesMoreThan500Counties1);
 		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+		
 		System.out.println("----------All States with more than 500 Counties Java 8-----------");
 		List<State> statesMoreThan500Counties2 = stateService.filter(states, (State s) -> s.getCounties() > 500);
 		System.out.println(statesMoreThan500Counties2);
 		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+		
 		System.out.println("----------All States with more than 200 Counties on East Coast Java 8-----------");
 		List<State> statesMoreThan500CountiesOnEastCoast 
 					= stateService.filter(states, (State s) -> {
@@ -49,6 +59,9 @@ public class StateService {
 					});
 		System.out.println(statesMoreThan500CountiesOnEastCoast);		
 		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+		
+		
 		System.out.println("----------Sort States By Name Java 7-----------");
 		Collections.sort(states, new Comparator<State>(){
 			@Override
@@ -58,6 +71,9 @@ public class StateService {
 		});		
 		System.out.println(states);
 		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+		
+		
 		System.out.println("----------Sort States By No Of Counties Java 7-----------");
 		Collections.sort(states, new Comparator<State>(){
 			@Override
@@ -67,22 +83,37 @@ public class StateService {
 		});		
 		System.out.println(states);
 		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+		
+		
 		System.out.println("----------Sort States By Name Java 8----1-------");
 		Collections.sort(states, (State a, State b) -> a.getName().compareTo(b.getName()));
 		System.out.println(states);
 		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+		
+		
 		System.out.println("----------Sort States By Name Java 8----2-------");
 		Collections.sort(states, comparing(State::getName));
 		System.out.println(states);
-		System.out.println("--------------------------------------------------------------------------------------------------------------");				
+		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+		
+		
 		System.out.println("----------Sort States By No Of Counties Java 8----1-------");
 		Collections.sort(states, (State a, State b) -> a.getCounties().compareTo(b.getCounties()));
 		System.out.println(states);
 		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+		
+		
 		System.out.println("----------Sort States By No Of Counties Java 8----2-------");
 		Collections.sort(states, comparing(State::getCounties));
 		System.out.println(states);
 		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+		
+		
 	}	
 	/**
 	 * 
