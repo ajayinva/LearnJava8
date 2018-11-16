be084f3fb4cb31dbe8b6431b71c0648ae2c98ba1
Ajay Agarwal <ajayinva@gmail.com>
2018-11-16 14:47:03 -0500

    State Local Commit

---
+++ src/com/aces/learn/java8/Test.java
@@ -0,0 +1,10 @@
+package com.aces.learn.java8;
+
+public class Test {
+
+  public static void main(final String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hello World");
+  }
+
+}
