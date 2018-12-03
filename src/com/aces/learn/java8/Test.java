bdf4c28c2c4f0c029b5141ef25627774eb287b66
Ajay Agarwal <ajayinva@gmail.com>
2018-12-03 11:31:35 -0500

    A remote change in the feature branch

--- src/com/aces/learn/java8/Test.java
+++ src/com/aces/learn/java8/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
   public static void main(final String[] args) {
     // New Changes in the feature branch
+    // A remote change in the feature branch
     System.out.println("Hello World");
   }
 
