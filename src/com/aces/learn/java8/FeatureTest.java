058258d7ee150b2702692a4502e6483fb77a7e3b
Ajay Agarwal <ajayinva@gmail.com>
2018-12-03 11:23:45 -0500

    A new feature has been commited

---
+++ src/com/aces/learn/java8/FeatureTest.java
@@ -0,0 +1,7 @@
+package com.aces.learn.java8;
+
+public class FeatureTest {
+  public static void main(final String[] args) {
+    System.out.println("Hello World in the Feature Test");
+  }
+}
--- src/com/aces/learn/java8/Test.java
+++ src/com/aces/learn/java8/Test.java
@@ -3,7 +3,7 @@ package com.aces.learn.java8;
 public class Test {
 
   public static void main(final String[] args) {
-    // Remote Changes1
+    // New Changes in the feature branch
     System.out.println("Hello World");
   }
 
