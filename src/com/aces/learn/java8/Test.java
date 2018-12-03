366f74a6ee03173da8fd9d05652d6063e4d55f5e
Ajay Agarwal <ajayinva@gmail.com>
2018-12-03 14:24:43 -0500

    Final Squashed Commit

--- src/com/aces/learn/java8/Test.java
+++ src/com/aces/learn/java8/Test.java
@@ -3,8 +3,7 @@ package com.aces.learn.java8;
 public class Test {
 
   public static void main(final String[] args) {
-    //Commit 3
-    // There is a change in the master branch
+    //Commit 13
     System.out.println("Hello World");
   }
 
