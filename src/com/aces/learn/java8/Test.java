9a8c2e100a64d1630ce01afc85672ef484fd594c
Ajay Agarwal <ajayinva@gmail.com>
2018-12-03 14:18:01 -0500

    Commit 1

--- src/com/aces/learn/java8/Test.java
+++ src/com/aces/learn/java8/Test.java
@@ -3,12 +3,7 @@ package com.aces.learn.java8;
 public class Test {
 
   public static void main(final String[] args) {
-    // New Changes in the feature branch
-    // This is local change in the feature branch
-    // A remote change in the feature branch
     //Commit 1
-    //Commit 2
-    //Commit 3
     System.out.println("Hello World");
   }
 
