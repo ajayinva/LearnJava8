b9a12436e917c074966ffa82196124dd86479c64
Ajay Agarwal <ajayinva@gmail.com>
2018-12-04 09:15:49 -0500

    Commit 1 into Master Branch

--- src/com/aces/learn/java8/Test.java
+++ src/com/aces/learn/java8/Test.java
@@ -3,9 +3,7 @@ package com.aces.learn.java8;
 public class Test {
 
   public static void main(final String[] args) {
-    //Commit 1 into feature Branch 002. This line also changed
-    //Commit 2 into feature Branch 002
-    //Commit 3 into feature Branch 002
+    //Commit 1 into Master Branch
     System.out.println("Hello World");
   }
 
