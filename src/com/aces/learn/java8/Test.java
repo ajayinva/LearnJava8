8c60abd8b2b9f9d577249cdd4a56bbd6aa3ed785
Ajay Agarwal <ajayinva@gmail.com>
2018-12-04 10:11:51 -0500

    Commit 1 after rebase

--- src/com/aces/learn/java8/Test.java
+++ src/com/aces/learn/java8/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
   public static void main(final String[] args) {
     //Commit 103 into Master Branch. This is a remote commit
+    //Commit 101 into Master Branch
     System.out.println("Hello World");
   }
 
