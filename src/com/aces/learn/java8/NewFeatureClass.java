2f53bd5822a8907a41e5203257314300dbb6040d
Ajay Agarwal <ajayinva@gmail.com>
2018-12-04 14:54:48 -0500

    This is commit 2 on the feature branch

--- src/com/aces/learn/java8/NewFeatureClass.java
+++ src/com/aces/learn/java8/NewFeatureClass.java
@@ -3,4 +3,5 @@ package com.aces.learn.java8;
 public class NewFeatureClass {
   //This is a new remote Commit
   //This is commit 1 on the feature branch
+  //This is commit 2 on the feature branch
 }
