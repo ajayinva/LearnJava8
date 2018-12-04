26b4038a939bd2bbbe1d546818b899aac45b60d4
Ajay Agarwal <ajayinva@gmail.com>
2018-12-04 14:54:48 -0500

    This is commit 2 on the feature branch
    Conflicts:
    	src/com/aces/learn/java8/NewFeatureClass.java

--- src/com/aces/learn/java8/NewFeatureClass.java
+++ src/com/aces/learn/java8/NewFeatureClass.java
@@ -2,4 +2,6 @@ package com.aces.learn.java8;
 
 public class NewFeatureClass {
   //This is a new remote Commit
+  //This is commit 1 on the feature branch
+  //This is commit 2 on the feature branch
 }
