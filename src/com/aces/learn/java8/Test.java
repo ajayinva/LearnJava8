e3de2dac9f634fd5d75e52ed98d763188ae03a96
Ajay Agarwal <ajayinva@gmail.com>
2018-12-04 13:33:08 -0500

    This is a remote commit on Master Branch. Another commit

--- src/com/aces/learn/java8/Test.java
+++ src/com/aces/learn/java8/Test.java
@@ -3,7 +3,7 @@ package com.aces.learn.java8;
 public class Test {
 
   public static void main(final String[] args) {
-    //This is a remote commit on Master Branch
+    //This is a remote commit on Master Branch. Another commit
     System.out.println("Hello World");
   }
 
