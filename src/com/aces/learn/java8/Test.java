66a29898869989696033af16878dd624c64367cb
Ajay Agarwal <ajayinva@gmail.com>
2018-12-04 10:19:57 -0500

    Commit 104 into Master Branch.

--- src/com/aces/learn/java8/Test.java
+++ src/com/aces/learn/java8/Test.java
@@ -3,7 +3,7 @@ package com.aces.learn.java8;
 public class Test {
 
   public static void main(final String[] args) {
-    //Commit 104 into Master Branch. This is a remote commit
+    //Commit 104 into Master Branch.
     System.out.println("Hello World");
   }
 
