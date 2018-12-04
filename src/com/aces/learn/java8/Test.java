ab018720386baafd7484f40a7decf3c77ab09e99
Ajay Agarwal <ajayinva@gmail.com>
2018-12-04 13:05:13 -0500

    This is a remote commit on Master Branch

--- src/com/aces/learn/java8/Test.java
+++ src/com/aces/learn/java8/Test.java
@@ -3,9 +3,7 @@ package com.aces.learn.java8;
 public class Test {
 
   public static void main(final String[] args) {
-    //Commit 101 into Feature Branch 005
-    //Commit 102 into Feature Branch 005
-    //Commit 103 into Feature Branch 005
+    //This is a remote commit on Master Branch
     System.out.println("Hello World");
   }
 
